a112555f92628b1b6ee5634e1896744e30f8103a
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-01-19 11:09:27 +0530

    main

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
-#exp1
-from exp1 import isprime
-isprime()
+# #exp1
+# from exp1 import isprime
+# isprime()
 
 # #exp2
 # from exp2 import ispalindrome
 # ispalindrome()
+
+#exp3
+from exp3 import exchange
+exchange()
