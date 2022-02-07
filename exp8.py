3fb8e10942afcff50bb448d7b9089db97ba6c45b
Kushagra Patidar <kushagra24ai027@satiengg.in>
2022-02-07 17:22:58 +0530

    main

--- exp8.py
+++ exp8.py
@@ -16,6 +16,7 @@ def binary_search():
     arr=input('Enter the elements of array with spaces: ').split()
     n=input('Enter the number to be searched: ')
     index=binarysearch(arr,0,len(arr)-1,n)
+    print(index)
     if index==0:
         print(f"{n} not found in the array")
     else:
